25e91da8fe672207e170b2770d1c8277175c3433
Alexander Sukhodolsky <alexander.sukhodolsky@pantheon.io>
2020-03-30 06:32:30 +0000

    test

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -32,3 +32,36 @@ $local_settings = __DIR__ . "/settings.local.php";
 if (file_exists($local_settings)) {
   include $local_settings;
 }
+
+// IPv4: Single IPs and CIDR.
+// See https://en.wikipedia.org/wiki/Classless_Inter-Domain_Routing
+$request_ip_blacklist = [
+  '192.0.2.38',
+  '192.0.3.125',
+  '192.0.67.0/30',
+  '192.0.78.0/24',
+];
+
+$request_remote_addr = $_SERVER['REMOTE_ADDR'];
+// Check if this IP is in black list.
+if (!$request_ip_forbidden = in_array($request_remote_addr, $request_ip_blacklist)) {
+  // Check if this IP is in CIDR black list.
+  foreach ($request_ip_blacklist as $_cidr) {
+    if (strpos($_cidr, '/') !== FALSE) {
+      $_ip = ip2long($request_remote_addr);
+      list ($_net, $_mask) = explode('/', $_cidr, 2);
+      $_ip_net = ip2long($_net);
+      $_ip_mask = ~((1 << (32 - $_mask)) - 1);
+
+      if ($request_ip_forbidden = ($_ip & $_ip_mask) == ($_ip_net & $_ip_mask)) {
+        break;
+      }
+    }
+  }
+}
+
+if ($request_ip_forbidden) {
+  header('HTTP/1.0 403 Forbidden');
+  //exit;
+  echo "Forbidden\n\n";
+}
